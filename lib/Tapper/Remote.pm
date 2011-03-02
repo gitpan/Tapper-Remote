4ee2215edb3a0aed3bab060f59321b2d791f701b
AMD OSRC (AMD Operating System Research Center) <tapper@tapper-testing.org>
2011-03-02 23:01:13 +0000

    Import of AMD/Tapper-Remote-3.000001 from CPAN.
    gitpan-cpan-distribution: Tapper-Remote
    gitpan-cpan-version:      3.000001
    gitpan-cpan-path:         AMD/Tapper-Remote-3.000001.tar.gz
    gitpan-cpan-author:       AMD
    gitpan-cpan-maturity:     released

---
+++ lib/Tapper/Remote.pm
@@ -0,0 +1,54 @@
+package Tapper::Remote;
+
+use warnings;
+use strict;
+use Moose;
+
+extends 'Tapper::Base';
+has cfg =>  (is => 'rw', isa => 'HashRef', default => sub {{mcp_port => 7357, mcp_host => 'localhost'}});
+
+sub BUILD
+{
+        my ($self, $config) = @_;
+        $self->{cfg}=$config;
+}
+
+
+=head1 NAME
+
+Tapper::Remote - Tapper - Common functionality for remote automation libs
+
+=cut
+
+our $VERSION = '3.000001';
+
+
+=head1 SYNOPSIS
+
+This module contains functions that are equal for all remote Tapper
+projects (currently Tapper::PRC and Tapper::Installer).
+Tapper::Remote itself does not export functionality but instead is the
+base image for all modules of the project.
+
+=head1 EXPORT
+
+Nothing. 
+
+=head1 FUNCTIONS
+
+=head1 AUTHOR
+
+AMD OSRC Tapper Team, C<< <tapper at amd64.org> >>
+
+=head1 BUGS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2011 AMD OSRC Tapper Team, all rights reserved.
+
+This program is released under the following license: freebsd
+
+=cut
+
+1; # End of Tapper::Remote
